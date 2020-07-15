954d37ff42e40e63e4c57fb3928b280419d0b51e
Oleksiyukraine <Oleksiyukraine@gmail.com>
2020-07-15 23:04:50 +0300

    Add bootstrap to project. See to event in react

--- src/App.js
+++ src/App.js
@@ -1,6 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Jumbotron from 'react-bootstrap/Jumbotron';
+import Toast from 'react-bootstrap/Toast';
+import Container from 'react-bootstrap/Container';
+import Button from 'react-bootstrap/Button';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const ExampleToast = ({ children }) => {
+  const [show, toggleShow] = useState(true);
+  return (
+      <>
+        {!show && <Button onClick={() => toggleShow(true)}>Show Toast</Button>}
+        <Toast show={show} onClose={() => toggleShow(false)}>
+          <Toast.Header>
+            <strong className="mr-auto">React-Bootstrap</strong>
+          </Toast.Header>
+          <Toast.Body>{children}</Toast.Body>
+        </Toast>
+      </>
+  );
+};
 
 function App() {
   return (
@@ -16,8 +36,19 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          React is here
         </a>
+        <div>
+          <Container className="p-3">
+            <Jumbotron>
+              <h1 className="header">Welcome To React-Bootstrap</h1>
+              <ExampleToast>
+                We now have Toasts
+                <span role="img" aria-label="tada">!+</span>
+              </ExampleToast>
+            </Jumbotron>
+          </Container>
+        </div>
       </header>
     </div>
   );
